9c4ad1a6bf9cd5a9a4d04f4d885426f3bab8a359
N.R.T <nathsharwor@hotmail.com>
2017-08-27 18:06:39 +0800

    Fixed gravity and implemented funcTriggers
    - Changed default global tick roughly 30Hz
    - Adjusted gravity constants
    - Renamed muzler.object to muzler.Object
    - Added muzler.Object.e, used to address the object's element
    - Changed default size of objects
    - Moved some classes from muzler to muzler.classes
    - Removed aero constants; aero physics scrapped
    - Fixed a bug that prevented adding forces correctly
    - Fixed a bug that made forces act in the wrong direction
    - Changed how VForce works and implemented it
    - Changed accuracy of some variables/displays to 3f
    - Implemented destruction of objects
    - Implemented object function events

--- muzler.js
+++ muzler.js
@@ -10,7 +10,7 @@ var muzler = {};
 
 //Stuff
 muzler.globalTickPort = -1;
-muzler.globalTickTime = 40;
+muzler.globalTickTime = 31; //Roughly 30hz
 
 //Used to store info such as screen dimensions. Almost preferences
 muzler.data = {};
@@ -27,6 +27,11 @@ Element.prototype.getElementById = function (id) {
 //Converts 'seconds' to ticks (mostly used for converting psec to ptick)
 Number.prototype.tps = function () {
 	return this * muzler.globalTickTime / 1000;
+};
+
+//Degrees to Rads
+Number.prototype.toRad = function () {
+	return this / 180 * Math.PI;
 };
 
 
@@ -47,20 +52,24 @@ muzler.Init = function (e) {
 //Includes all classes/objects
 muzler.classes = {};
 
+//Includes (most) helper functions
+muzler.funcs = {};
+
 //TODO
 muzler.classes.PhysicsInput = function () {
 	//_PRIVATE_
 	this.objectList = [];
 	
 	//Stats
-	this.gravity = 9.81; //DEFAULT
+	this.gravity = 9.81 * 0.8; //DEFAULT
+	this.scale = 5; //pixel to metre ratio
 
 	//User callable functions
 	this.add = {};
 	this.add.p = this; //Passing parent... DEFINATELY NOT BAD PRACTICE
 	this.add.object = function (obj) {
-		if (!(obj instanceof muzler.object)) {
-			muzler.out.error("Passed object not of type 'muzler.object'. Object: " + JSON.stringify(obj));
+		if (!(obj instanceof muzler.Object)) {
+			muzler.out.error("Passed object not of type 'muzler.Object'. Object: " + JSON.stringify(obj));
 			return false;
 		}
 		this.p.objectList.push(obj);
@@ -73,22 +82,23 @@ Array.prototype.idIndex = function (id) {
 };
 
 //TODO
-muzler.object = function (id) {
+muzler.Object = function (id) {
 	//_PRIVATE_
 	this._del = false;
 	
 	this.id = id || Math.floor(Math.random() * 1000000);
+	this.e = undefined;
 	
 	this.x = 50;
 	this.y = 50;
 	this.r = 0;
 
-	this.dx = 50; //DEFAULT
-	this.dy = 50; //DEFAULT
+	this.dx = 25; //DEFAULT
+	this.dy = 25; //DEFAULT
 	this.imgSrc = "black"; //DEFAULT
 
-	this.physics = new muzler.PhysicsEngine();
-	this.functions = new muzler.FunctionHandler();
+	this.physics = new muzler.classes.PhysicsEngine();
+	this.functions = new muzler.classes.FunctionHandler();
 	
 	//Functions
 	this.destroy = function () { this._del = true; }
@@ -126,7 +136,7 @@ for (var i = 0; i < Object.keys(muzler.out._rel).length; i++)
 	muzler.out[Object.keys(muzler.out._rel)[i]] = eval("(function (m) { muzler.out._base(m, '" + Object.keys(muzler.out._rel)[i] + "'); })");
 
 //Contains physics functions and attributes used by objects when ticking
-muzler.PhysicsEngine = function () {
+muzler.classes.PhysicsEngine = function () {
 	//Physics relaed stats. All are in SI
 	this.stats = {};
 	this.stats.mass = 100; //DEFAULT
@@ -134,7 +144,6 @@ muzler.PhysicsEngine = function () {
 	//Multipliers to manualy adjust certain physics
 	this.multipliers = {};
 	this.multipliers.gravity = 1; //DEFAULT
-	this.multipliers.aero = 1; //DEFAULT
 
 	//What do you think?
 	this.gravity = {};
@@ -144,27 +153,25 @@ muzler.PhysicsEngine = function () {
 	
 	//Used to make it move
 	this.forces = [];
-	this.force = function (s, d, m) { this.forces.push(f == undefined ? new muzler.Force(s, d) : new muzler.VForce(s, d, m)); };
+	this.force = function (s, d, n) { this.forces.push(typeof s == "function" ? new muzler.classes.VForce(s, d, n) : new muzler.classes.Force(s, d, n)); };
 };
 
 //Used to define a force enacting upon an object
-muzler.Force = function (s, d, id) {
-	this.id = id || Math.floor(Math.random() * 1000000);
-	this.direction = d || 0; //DEFAULT
-	this.strength = s || 1; //DEFAULT
+muzler.classes.Force = function (s, d, n) {
+	this.name = n || Math.floor(Math.random() * 1000000);
+	this.direction = Math.round(d) || 0; //DEFAULT
+	this.strength = (Math.round(s * 1000) / 1000) || 1; //DEFAULT
 };
 
 //Used to define a force enacting upon an object that changes relative to the object's velocity
-muzler.VForce = function (s, d, m, id) {
-	this.id = id || Math.floor(Math.random() * 1000000);
-	this.direction = d || 0; //DEFAULT
-	this.strength = s || 1; //DEFAULT
-	this.multiplier = m || 0; //DEFAULT - SHOULD BE CHANGED ON DECLARATION
+muzler.classes.VForce = function (f, d, n) {
+	this.name = n || Math.floor(Math.random() * 1000000);
+	this.direction = Math.round(d) || 0; //DEFAULT
+	this.func = f || function (m, d) { return 1; }; //DEFAULT - SHOULD BE CHANGED ON DECLARATION
 };
 
 //Functions triggered at certain calculation times
-//g defines the scope for the function to operate at. Defaults to false. True means global, false means local (the parent muzler.Object())
-muzler.FunctionHandler = function () {
+muzler.classes.FunctionHandler = function () {
 	//Creation
 	this.preSpawn = [];
 	this.postSpawn = [];
@@ -175,7 +182,13 @@ muzler.FunctionHandler = function () {
 	
 	//Tick
 	this.preTick = [];
-	this.postTick = [];
+	this.postTick = []; //Not in place yet
+};
+
+//Used to trigger objects' event functions
+muzler.Object.prototype.funcTriggerEvent = function (t) {
+	for (var n = 0; n < this.functions[t].length; n++)
+		this.functions[t][n](this);
 };
 
 //Function called at or after window.load
@@ -215,7 +228,7 @@ muzler.init = function (mio) {
 		
 		if (muzler.globalTickPort != -1) {
 			muzler.out.sub("Global tick successfully iniialised on port " + muzler.globalTickPort + ".");
-			muzler.out.sub("Global tick operating at " + (1000 / muzler.globalTickTime) + "Hz - " + muzler.globalTickTime + "msec.");
+			muzler.out.sub("Global tick operating at " + Math.floor(10000 / muzler.globalTickTime) / 10 + "Hz - " + muzler.globalTickTime + "msec.");
 		} else
 			muzler.out.fatal("Failed to initialise global tick. Reason Unknown.");
 	} catch (err) { muzler.out.fatal("Failed to initialise global tick: '" + err + "'"); }
@@ -226,10 +239,14 @@ muzler.init = function (mio) {
 //Global Tick event. Calls for all physics to be updated aor calculated.
 muzler.globalTick = function () {
 	if (!!muzler.data.engine) {
-		objs = muzler.data.engine.objectList;
+		var objs = muzler.data.engine.objectList;
+		var delArr = [];
 		
 		for (var i = 0; i < objs.length; i++) {
-			if (!muzler.element.getElementById(objs[i].id)) {
+			if (!objs[i].e) {
+				objs[i].funcTriggerEvent("preSpawn");
+				
+				//Spawn
 				o = document.createElement("span");
 				o.id = objs[i].id;
 				o.style.position = "absolute";
@@ -239,24 +256,59 @@ muzler.globalTick = function () {
 				o.style.left = objs[i].x;
 				o.style.background = objs[i].imgSrc;
 				muzler.element.appendChild(o);
+				objs[i].e = o;
+				
+				objs[i].funcTriggerEvent("postSpawn");
+			} else if (objs[i]._del) {
+				//PreDestroy trigger
+				objs[i].funcTriggerEvent("preDestroy");
+				
+				//Destroy
+				muzler.element.removeChild(objs[i].e);
+				delArr.push(objs[i].id);
+				
+				//PostDestroy trigger
+				objs[i].funcTriggerEvent("postDestroy");
 			} else {
-				e = muzler.element.getElementById(objs[i].id);
+				objs[i].funcTriggerEvent("preTick");
 				
 				//Gravity
 				if (objs[i].physics.gravity.direction != -1)
-					objs[i].physics.force(muzler.data.engine.gravity.tps() * objs[i].physics.multipliers.gravity, objs[i].physics.gravity.direction); //PROBLEM WITH .force
+					objs[i].physics.force(muzler.data.engine.gravity.tps() * objs[i].physics.multipliers.gravity, objs[i].physics.gravity.direction, "gravity");
+				
+				//Clear all gravity
+				//objs[i].physics.forces = objs[i].physics.forces.filter(function (f) { return f.name != "gravity"; });
 				
 				//Calc
 				var dx = 0;
 				var dy = 0;
 				for (var n = 0; n < objs[i].physics.forces.length; n++) {
-					dx += objs[i].physics.forces[n].strength * Math.sin(objs[i].physics.forces[n].direction);
-					dy += objs[i].physics.forces[n].strength * Math.cos(objs[i].physics.forces[n].direction);
+					if (objs[i].physics.forces[n] instanceof muzler.classes.Force) {
+						var nm = objs[i].physics.forces[n].strength;
+						var nd = objs[i].physics.forces[n].direction.toRad();
+						dx += Math.floor(nm * Math.sin(nd) * 1000) / 1000;
+						dy += Math.floor(nm * Math.cos(nd) * 1000) / 1000;
+					}
 				}
-				e.style.top = parseInt(e.style.top.substr(0, e.style.top.length - 2)) + dy + "px"
-				e.style.left = parseInt(e.style.left.substr(0, e.style.left.length - 2)) + dx + "px";
+				
+				var d = Math.atan2(dx, dy);
+				var m = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+				for (var n = 0; n < objs[i].physics.forces.length; n++) {
+					if (objs[i].physics.forces[n] instanceof muzler.classes.VForce) {
+						var nm = Math.round(objs[i].physics.forces[n].func(m, d) * 1000) / 1000;
+						var nd = objs[i].physics.forces[n].direction.toRad();
+						dx += Math.floor(nm * Math.sin(nd) * 1000) / 1000;
+						dy += Math.floor(nm * Math.cos(nd) * 1000) / 1000;
+					}
+				}
+				
+				objs[i].e.style.top = parseInt(objs[i].e.style.top.substr(0, objs[i].e.style.top.length-2))+Math.round(dy*muzler.data.engine.scale)+"px";
+				objs[i].e.style.left = parseInt(objs[i].e.style.left.substr(0, objs[i].e.style.left.length-2))+Math.round(dx*muzler.data.engine.scale)+"px";
 			}
 		}
+		
+		//Remove destroyed objects from the object list
+		muzler.data.engine.objectList = objs.filter(new Function ("e", "return ![" + delArr + "].includes(e.id);"));
 	}
 };
 
